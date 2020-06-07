e78d6b9f1cb87017400b05804dec6274465dd94c
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2020-06-07 17:30:44 -0600

    finishing

---
+++ portfolio/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Portfolio Home Page!")
+
+def contact(request):
+    return HttpResponse("Contact Me")
+
+def greet_by_name(request, name):
+    return HttpResponse(f"Hello {name}! Welcome!")
